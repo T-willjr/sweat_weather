850e570112f93290ada2f351cbaf87fe21426065
TJ Williams <williamsjr.timothy1@gmail.com>
2022-04-23 20:25:25 -0400

    Add: Geocode Facade/Poros/Services/Serialzier

---
+++ app/facades/geocode_facade.rb
@@ -0,0 +1,15 @@
+class GeocodeFacade 
+  class << self 
+    def get_lat_long(city) 
+      data = GeocodeService.call_lat_long(city)
+      latitude_longitude(data)
+    end 
+
+    private 
+
+    def latitude_longitude(geocode_data)
+      lat_long = geocode_data[:results].last[:locations].first[:latLng]
+      CityLatLong.new(latitude: lat_long[:lat], longitude: lat_long[:lng])
+    end 
+  end
+end 
---
+++ app/poros/city_lat_long.rb
@@ -0,0 +1,8 @@
+class CityLatLong 
+  attr_reader :latitude, :longitude 
+  
+  def initialize(data)
+    @latitude = data[:latitude]
+    @longitude = data[:longitude]
+  end 
+end 
