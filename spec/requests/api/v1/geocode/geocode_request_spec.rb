f3c381a6e0d5320dd8cc24c281c2345a15482a8c
TJ Williams <williamsjr.timothy1@gmail.com>
2022-04-25 12:56:58 -0400

    Test: Endpoint can get travel time between two cities

--- spec/requests/api/v1/geocode/geocode_request_spec.rb
+++ spec/requests/api/v1/geocode/geocode_request_spec.rb
@@ -16,4 +16,17 @@ RSpec.describe "Gets Longitude/Latitude for a ctiy" do
     expect(geocode_result.values.first).to be_a Float
     expect(geocode_result.values.last).to be_a Float
   end 
+
+  it "gets travel time from city to city" do  
+    start_location = "denver,co"
+    end_location = "pueblo,co"
+
+
+    get "/api/v1/geocode/route/time?start=#{start_location}&end=#{end_location}"
+
+    time_result = JSON.parse(response.body, symbolize_names: true)
+    expect(response).to be_successful
+    expect(time_result.count).to eq(1)
+    expect(time_result[:data][:attributes].keys).to eq([:travel_time])
+  end 
 end 
