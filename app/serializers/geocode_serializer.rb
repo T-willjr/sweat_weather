5452de289243ee248269692059bdfabf73a44f03
TJ Williams <williamsjr.timothy1@gmail.com>
2022-04-25 12:58:05 -0400

    Add: Geocode facade/service/serialzier for route time

--- app/serializers/geocode_serializer.rb
+++ app/serializers/geocode_serializer.rb
@@ -2,4 +2,17 @@ class GeocodeSerializer
   include JSONAPI::Serializer
   attributes :id, :latLng
 
+  class << self 
+    def time_hash(data)
+      {
+        "data": {
+          "id": "null",
+          "type": "time",
+          "attributes": {
+            "travel_time": data[:route][:realTime]
+       }
+     }
+  }
+    end 
+  end 
 end
--- app/services/geocode_service.rb
+++ app/services/geocode_service.rb
@@ -5,10 +5,15 @@ class GeocodeService
       parse_data(response)
     end
 
+    def call_route_time(start,destination)
+      response = conn.post("/directions/v2/route?key=#{ENV['api_key']}&from=#{start}&to=#{destination}")
+      a = parse_data(response)
+    end 
+
     def conn 
       Faraday.new(url: "http://www.mapquestapi.com")
     end 
-
+   
     def parse_data(response)
       data = JSON.parse(response.body, symbolize_names: true)
     end 
