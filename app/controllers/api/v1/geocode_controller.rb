de906a58e78c23585a3fd154ae2ee976ef42b897
TJ Williams <williamsjr.timothy1@gmail.com>
2022-04-25 12:57:40 -0400

    Add: Geocode Routes & action in controller

--- app/controllers/api/v1/geocode_controller.rb
+++ app/controllers/api/v1/geocode_controller.rb
@@ -3,4 +3,10 @@ class Api::V1::GeocodeController < ApplicationController
     city = params[:city]
     render json: GeocodeFacade.get_lat_long(city)
   end 
+
+  def time
+    start_location = params[:start]
+    destination = params[:end]
+    render json: GeocodeSerializer.time_hash(GeocodeFacade.get_route_time(start_location, destination))
+  end 
 end 
