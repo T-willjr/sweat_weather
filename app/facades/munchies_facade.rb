9237ca4554316ce9280336c571c053406b7b12cb
TJ Williams <williamsjr.timothy1@gmail.com>
2022-04-25 12:59:03 -0400

    Add: Geocode facade to grab time in munchies facade

--- app/facades/munchies_facade.rb
+++ app/facades/munchies_facade.rb
@@ -1,15 +1,22 @@
 class MunchiesFacade 
   class << self
     def get_food_forecast(start, destination, food)
-      restaurant_data = MunchiesService.call_restaurant_info(food, destination)
-      # travel_data = GeocodeService.call_travel_time(start, destination)
-      # forecast_data = ForecastFacade.get_weather(destination)
+      time = route_time(start,destination)
+      time_for_restaurant = Time.now.to_i + time 
+      restaurant_data = MunchiesService.call_restaurant_info(food, destination, time_for_restaurant)
+      binding.pry
+      #forecast_data = ForecastFacade.get_weather(destination)
     end 
 
     private 
+    def route_time(start, destination) 
+      route_time = GeocodeSerializer.time_hash(GeocodeFacade.get_route_time(start, destination))
+      time_in_seconds = route_time[:data][:attributes][:travel_time]
+    end 
 
     def food_forecast_object(forecast_data, restaurant_data, travel_data)
 
     end 
   end 
 end 
+
