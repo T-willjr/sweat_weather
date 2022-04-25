3533880013aa939fe321bade23fb1f5586a02c15
TJ Williams <williamsjr.timothy1@gmail.com>
2022-04-25 13:56:11 -0400

    Update: Food_forecast test

--- spec/requests/api/v1/food_forecast/food_forecast_request_spec.rb
+++ spec/requests/api/v1/food_forecast/food_forecast_request_spec.rb
@@ -6,16 +6,28 @@ RSpec.describe "Gets Food & Forecast for a ctiy" do
     city = "pueblo,co"
     start = "denver,co"
     food = "chinese"
+    data_keys = :id, :type, :attributes
+    attribute_keys = [:destination_city, :travel_time, :forecast, :restaurant]
+    forecast_keys = [:summary, :temperature]
+    restaurant_keys = [:name, :address]
 
     get "/api/v1/munchies?start=#{start}&destination=#{city}&food=#{food}"
 
     food_forecast_result = JSON.parse(response.body, symbolize_names: true)
 
     expect(response).to be_successful
+
     expect(food_forecast_result.count).to eq(1)
     expect(food_forecast_result[:data].keys.count).to eq(3)
     expect(food_forecast_result[:data].keys).to eq(data_keys)
-    expect(food_forecast_result[:data][:attributes][:image].keys.count).to eq(2)
-    expect(food_forecast_result[:data][:attributes][:image].keys).to eq(image_keys)
+
+    expect(food_forecast_result[:data][:attributes].keys.count).to eq(4)
+    expect(food_forecast_result[:data][:attributes].keys).to eq(attribute_keys)
+
+    expect(food_forecast_result[:data][:attributes][:forecast].keys.count).to eq(2)
+    expect(food_forecast_result[:data][:attributes][:forecast].keys).to eq(forecast_keys)
+
+    expect(food_forecast_result[:data][:attributes][:restaurant].keys.count).to eq(2)
+    expect(food_forecast_result[:data][:attributes][:restaurant].keys).to eq(restaurant_keys)
   end 
 end 
