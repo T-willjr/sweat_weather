c7745b73e2205803e714b72a1e8f8b40d159b131
TJ Williams <williamsjr.timothy1@gmail.com>
2022-04-26 21:39:20 -0400

    Test: Creates a user session happy/sad path

---
+++ spec/requests/api/v1/sessions/create_user_sessions_request_spec.rb
@@ -0,0 +1,99 @@
+require 'rails_helper'
+
+RSpec.describe "Creates A User Session" do  
+  it "creates one user session" do 
+    data_keys = [:type, :id, :attributes]
+    attribute_keys = [:email, :api_key]
+    user = User.create!(email: "test@email.com", password: "12345", password_confirmation: "12345")
+
+    user_params = 
+        { 
+           "email": "test@email.com",
+           "password": "12345", 
+           "password_confirmation": "12345"
+        }
+     
+ 
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post "/api/v1/sessions", headers: headers, params: JSON.generate(user: user_params)
+    
+    user_result = JSON.parse(response.body, symbolize_names: true)
+    
+    expect(response).to have_http_status(200)
+    
+		expect(user.email).to eq(user_params[:email])
+		expect(user.id).to eq(user_result[:data][:id])
+
+
+    expect(user_result.count).to eq(1)
+    expect(user_result[:data].keys.count).to eq(3)
+    expect(user_result[:data].keys).to eq(data_keys)
+
+    expect(user_result[:data][:attributes].keys.count).to eq(2)
+    expect(user_result[:data][:attributes].keys).to eq(attribute_keys)
+  end 
+
+  it "will not get user session if passwords do not match and returns error response" do 
+    data_keys = [:type, :id, :attributes]
+    attribute_keys = [:error]
+    successful_keys = [:email, :api_key]
+    user = User.create!(email: "test@email.com", password: "12345", password_confirmation: "12345")
+
+    user_params = 
+        { 
+           "email": "test@email.com",
+           "password": "12345", 
+           "password_confirmation": "123456789"
+        }
+     
+ 
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post "/api/v1/sessions", headers: headers, params: JSON.generate(user: user_params)
+
+    error_response = JSON.parse(response.body, symbolize_names: true)
+    #expect(response).to have_http_status(201)
+
+    expect(error_response.count).to eq(1)
+    expect(error_response[:data].keys.count).to eq(3)
+    expect(error_response[:data].keys).to eq(data_keys)
+
+    expect(error_response[:data][:attributes].keys.count).to eq(1)
+    expect(error_response[:data][:attributes].keys).to eq(attribute_keys)
+    expect(error_response[:data][:attributes].keys).to_not eq(successful_keys)
+    expect(error_response[:data][:attributes][:error][:message]).to eq("Your email or password is incorrect")
+  end
+
+  it "will not get user session if email does not exist and returns error response" do 
+    data_keys = [:type, :id, :attributes]
+    attribute_keys = [:error]
+    successful_keys = [:email, :api_key]
+    user = User.create!(email: "test@email.com", password: "12345", password_confirmation: "12345")
+
+    user_params = 
+        { 
+           "email": "",
+           "password": "12345", 
+           "password_confirmation": "12345"
+        }
+     
+ 
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post "/api/v1/sessions", headers: headers, params: JSON.generate(user: user_params)
+
+    error_response = JSON.parse(response.body, symbolize_names: true)
+    #expect(response).to have_http_status(201)
+    
+
+    expect(error_response.count).to eq(1)
+    expect(error_response[:data].keys.count).to eq(3)
+    expect(error_response[:data].keys).to eq(data_keys)
+
+    expect(error_response[:data][:attributes].keys.count).to eq(1)
+    expect(error_response[:data][:attributes].keys).to eq(attribute_keys)
+    expect(error_response[:data][:attributes].keys).to_not eq(successful_keys)
+    expect(error_response[:data][:attributes][:error][:message]).to eq("Your email or password is incorrect")
+  end
+end 
