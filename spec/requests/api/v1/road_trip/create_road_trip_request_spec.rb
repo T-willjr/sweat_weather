e497d35a8f75b47e1f31e3fa918f4d6437368954
TJ Williams <williamsjr.timothy1@gmail.com>
2022-04-27 08:01:47 -0400

    Test: Travel time should be impossible for certain requests

--- spec/requests/api/v1/road_trip/create_road_trip_request_spec.rb
+++ spec/requests/api/v1/road_trip/create_road_trip_request_spec.rb
@@ -66,4 +66,43 @@ RSpec.describe "Gets Road Trip Results" do
     expect(error_response[:data][:attributes].keys).to_not eq(successful_keys)
     expect(error_response[:data][:attributes][:error][:message]).to eq("Unauthorized api key")
   end
+
+
+  it "travel time should be 'impossible' and weather blank when travel from country to country" do  
+    data_keys = [:id, :type, :attributes]
+    attribute_keys = [:start_city, :end_city, :travel_time, :weather_at_eta]
+    weather_at_eta_keys = [:temperature, :conditions]
+    restaurant_keys = [:name, :address]
+
+    user = User.create!(email: "test@email.com", password: "12345", password_confirmation: "12345")
+    road_trip_params =
+      {
+        "origin": "New York,NY",
+        "destination": "London,UK",
+        "api_key": user.private_api_key
+      }
+
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post "/api/v1/road_trip", headers: headers, params: JSON.generate(road_trip: road_trip_params)
+
+    road_trip_result = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+
+    expect(road_trip_result.count).to eq(1)
+    expect(road_trip_result[:data].keys.count).to eq(3)
+    expect(road_trip_result[:data].keys).to eq(data_keys)
+
+    expect(road_trip_result[:data][:attributes].keys.count).to eq(4)
+    expect(road_trip_result[:data][:attributes].keys).to eq(attribute_keys)
+
+    expect(road_trip_result[:data][:attributes][:weather_at_eta].keys.count).to eq(2)
+    expect(road_trip_result[:data][:attributes][:weather_at_eta].keys).to eq(weather_at_eta_keys)
+    
+    expect(road_trip_result[:data][:attributes][:travel_time]).to eq("Impossible")
+
+    expect(road_trip_result[:data][:attributes][:weather_at_eta][:temperature]).to eq("")
+    expect(road_trip_result[:data][:attributes][:weather_at_eta][:conditions]).to eq("")
+  end 
 end 
